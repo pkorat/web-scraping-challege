99c4d7fe00ab1165a956776b76070166b10700d9
pkorat <parthkorat95@gmail.com>
2021-08-22 18:50:39 -0700

    created app.py

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -3,8 +3,6 @@ from bs4 import BeautifulSoup as bs
 from splinter import Browser
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
-from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
 
 
 def scrape():
@@ -75,6 +73,3 @@ def scrape():
     scraped_data['Hemisphere Images'] = hemisphere_image_urls
 
     return scraped_data
-
-app = Flask(__name__)
-
